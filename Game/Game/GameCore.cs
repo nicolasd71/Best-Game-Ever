645c085cd571435142e8d4714100670409ced892
AshkoreDracson <AshkoreD@gmail.com>
2016-03-13 17:49:46 +0100

    Added basic game loop & other stuff

---
+++ Game/Game/GameCore.cs
@@ -0,0 +1,34 @@
+﻿namespace Game
+{
+    public static class GameCore
+    {
+        public static void Init()
+        {
+            // Init some other stuff
+            Start();
+        }
+
+        public static void Start()
+        {
+            // Start some stuff, more init
+            Update();
+        }
+
+        public static void Update()
+        {
+            while (true) // Replace true with Game.WindowOpened or something
+            {
+                // Do loop
+                // Maybe implement targetFps property in order to limit FPS (VSync-like)
+                Time.FinishUpdate(); // Call this only when update loop is finished.
+            }
+
+            // The loop has exited, end the bloody game
+        }
+
+        public static void Render()
+        {
+            // Render stuff here, idk
+        }
+    }
+}
--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -11,6 +11,7 @@ namespace Game
     {
         static void Main(string[] args)
         {
+            GameCore.Init(); // Maybe handle RenderWindow creation from Init() ?
             SFML.Graphics.RenderWindow w = new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(800, 600), "Test");
             Console.ReadLine();
         }
