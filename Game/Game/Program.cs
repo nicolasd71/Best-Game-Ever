c9fb5e2e4c1fcb21a5199cce6e17db3413c4c7e1
Yui Asuna <psych0tyk@hotmail.fr>
2016-03-13 17:15:44 +0100

    SFML

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML;
 
 namespace Game
 {
@@ -10,6 +11,8 @@ namespace Game
     {
         static void Main(string[] args)
         {
+            SFML.Graphics.RenderWindow w = new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(800, 600), "Test");
+            Console.ReadLine();
         }
     }
 }
