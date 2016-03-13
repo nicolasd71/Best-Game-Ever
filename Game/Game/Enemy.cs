113df83f95f7b4e0cb3d32bc9ed4d87bf7932e43
AshkoreDracson <AshkoreD@gmail.com>
2016-03-13 19:43:00 +0100

    Enemy base class

---
+++ Game/Game/Enemy.cs
@@ -0,0 +1,17 @@
+﻿namespace Game
+{
+    public class Enemy : GameObject
+    {
+        public float health { get; set; } = 100f;
+
+        public override void Start()
+        {
+            
+        }
+
+        public override void Update()
+        {
+            
+        }
+    }
+}
