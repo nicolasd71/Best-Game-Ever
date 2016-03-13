ebb3a4e98d53e39dca3a0c7df25fa6e3c194241e
Yui Asuna <psych0tyk@hotmail.fr>
2016-03-13 17:25:14 +0100

    Renderer

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using SFML;
 
 namespace Game
@@ -11,8 +7,7 @@ namespace Game
     {
         static void Main(string[] args)
         {
-            SFML.Graphics.RenderWindow w = new SFML.Graphics.RenderWindow(new SFML.Window.VideoMode(800, 600), "Test");
-            Console.ReadLine();
+            
         }
     }
 }
